ae31039f691c002613b0cb57da2953a8ff8d3b5f
mashalip <lip357@mail.ru>
2025-01-27 22:38:40 +0300

    Add brusnika.py

---
+++ brusnika.py
@@ -0,0 +1,48 @@
+print('брусника маринованная')
+
+brusnika_marin = {"brusnika" : 300, "sugar" : 100, "salt" : 120, "water" : 100}
+print(brusnika_marin)
+
+if __name__ == '__main__':
+    delitel = 1
+
+    for ingridient, weit in brusnika_marin.items():
+        print(ingridient, weit)
+
+        quantity = input()
+        if quantity == "":
+            continue
+
+        quantity = int(quantity)
+
+        if  quantity == 0 or quantity == weit:
+            continue
+
+        else:
+        #if quantity != weit:
+            delitel = weit / quantity
+            print(delitel)
+            break
+
+    for ingridient, weit in brusnika_marin.items():
+        weit /= delitel
+        print(ingridient, weit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
